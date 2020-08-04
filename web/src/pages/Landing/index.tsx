70f5b7195ca12e179aad27922ec32016170affb6
DeborahNascimento <deborahmdn@hotmail.com>
2020-08-04 15:20:56 -0300

    update codigo

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
-
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import './styles.css';
 function Landing(){
     return (
         <div id="page-landing">
@@ -11,7 +14,23 @@ function Landing(){
                     <h2>Sua plataforma de estudos online</h2>
                 </div>
 
-                <img src={landingImg} alt=""/>
+                <img src={landingImg} alt="" className="hero=image"/>
+
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="estudar" />
+                        Estudar
+                    </a>
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="estudar"/>
+                        Estudar
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizada <img src={purpleHeartIcon} alt="coração roxo"/>
+                </span>
+                
             </div>
         </div>
 
