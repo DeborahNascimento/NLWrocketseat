fac96ee3a9c71865c2d456e6404a8bcb8fae360b
DeborahNascimento <deborahmdn@hotmail.com>
2020-08-05 22:39:36 -0300

    update

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
-
-import './styles.css';
-
 function TeacherForm(){
     return(
         <div id="page-teacher-form" className="container">
