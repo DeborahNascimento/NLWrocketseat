bd6291c570ecdee8a645fa9b84392bd801459f94
DeborahNascimento <deborahmdn@hotmail.com>
2020-08-05 23:03:53 -0300

    update

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-
+import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
@@ -30,6 +30,31 @@ function TeacherList(){
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars2.githubusercontent.com/u/9783209?s=460&u=860883c4d009b4832dda95da7c5a08a5aabfd4db&v=4" />
+                        <div>
+                            <strong>Deborah Medeiros</strong>
+                            <span>Redes de computadores</span>
+                        </div>
+                    </header>
+                    <p>
+                        gosto de rackear facebooks e ver o que o povo tá fuçando na web enquanto deveria tá trabalhando
+                    </p>
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsAppIcon} alt="whatsapp" />
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
 
     )
